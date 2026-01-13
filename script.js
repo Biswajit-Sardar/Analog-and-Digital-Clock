f4e230eb40631ff461ea0564a2d11ae5bc00400d
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2026-01-13 22:11:27 +0530

    Add initial implementation of digital clock with HTML, CSS, and JavaScript

---
+++ script.js
@@ -0,0 +1,35 @@
+function myTime(){
+    var myDate= new Date();
+     min = (myDate.getMinutes()<10) ? "0" + myDate.getMinutes() :myDate.getMinutes(),
+        sec = (myDate.getSeconds() <10 ) ? "0" + myDate.getSeconds() : myDate.getSeconds(),
+        M = (myDate.getHours() >= 12) ? "PM" : "AM";
+        if(myDate.getHours() ==0){
+            hr=12;
+        }else if (myDate.getHours() >12){
+                    hr =  myDate.getHours() -12;
+        } else{
+            hr = myDate.getHours();
+        }
+
+
+        /*currentTime=hr + ":" + min + ":" +sec +""; 
+        document.getElementsByClassName("H")[0].innerHTML=currentTime;*/
+        document.getElementsByClassName("hour")[0].innerHTML=hr;
+         document.getElementsByClassName("min")[0].innerHTML=min;
+          document.getElementsByClassName("sec")[0].innerHTML=sec;
+        document.getElementsByClassName("M")[0].innerHTML= M;
+        myDay=[ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        myMonth=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        day= myDate.getDate();
+
+       /* currentDate= myDay[myDate.getDay()] + ", "+ " " + day + " " + myMonth[myDate.getMonth()] + " " + myDate.getFullYear();  ;
+        document.getElementsByClassName("Date")[0].innerHTML=currentDate; */
+        document.getElementsByClassName("day")[0].innerHTML= myDay[myDate.getDay()];
+        document.getElementsByClassName("month")[0].innerHTML= day + ", " + myMonth[myDate.getMonth()];
+        document.getElementsByClassName("year")[0].innerHTML= myDate.getFullYear();
+}
+var hr, min,sec, M, currentDate, currentTime, myDate, myMonth, day, myDay;
+
+
+myTime();
+setInterval(myTime,1000)
